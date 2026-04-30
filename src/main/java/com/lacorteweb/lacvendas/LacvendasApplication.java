a0615206dcbc6ea7006c5c1fb3428ac0e0152371
dlacorte10 <daniel.lacorte10@gmail.com>
2026-04-30 20:38:10 -0300

    Project created

---
+++ src/main/java/com/lacorteweb/lacvendas/LacvendasApplication.java
@@ -0,0 +1,13 @@
+package com.lacorteweb.lacvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LacvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LacvendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lacorteweb/lacvendas/LacvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lacorteweb.lacvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LacvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
