8587393435da193d8ae917f93a8396af157d4662
dlacorte10 <daniel.lacorte10@gmail.com>
2026-05-04 08:54:15 -0300

    Payment, one-to-one association

--- src/main/java/com/lacorteweb/lacvendas/config/TestConfig.java
+++ src/main/java/com/lacorteweb/lacvendas/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.lacorteweb.lacvendas.entities.Category;
 import com.lacorteweb.lacvendas.entities.Order;
 import com.lacorteweb.lacvendas.entities.OrderItem;
+import com.lacorteweb.lacvendas.entities.Payment;
 import com.lacorteweb.lacvendas.entities.Product;
 import com.lacorteweb.lacvendas.entities.User;
 import com.lacorteweb.lacvendas.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
