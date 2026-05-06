43d924f785eca2bd5df14808c96add0d53ec7140
dlacorte10 <daniel.lacorte10@gmail.com>
2026-05-06 14:28:45 -0300

    Exception handling - update

--- src/main/java/com/lacorteweb/lacvendas/services/UserService.java
+++ src/main/java/com/lacorteweb/lacvendas/services/UserService.java
@@ -13,6 +13,8 @@ import com.lacorteweb.lacvendas.repositories.UserRepository;
 import com.lacorteweb.lacvendas.services.exceptions.DatabaseException;
 import com.lacorteweb.lacvendas.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -46,9 +48,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+
 	}
 
 	private void updateData(User entity, User obj) {
